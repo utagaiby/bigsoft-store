955e4f34ecdd3423bba526c5ab2d3e1b947e9fee
vagrant <vagrant@precise32.(none)>
2013-12-02 16:26:46 +0000

    [lesson 2] Product list and create

--- src/Bigsoft/StoreBundle/Controller/ProductController.php
+++ src/Bigsoft/StoreBundle/Controller/ProductController.php
@@ -9,12 +9,37 @@
 namespace Bigsoft\StoreBundle\Controller;
 
 
+use Bigsoft\StoreBundle\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ProductController extends Controller
 {
-    public function listAction() {
+    public function listAction()
+    {
         $products = $this->getDoctrine()->getRepository('BigsoftStoreBundle:Product')->findAll();
         return $this->render('BigsoftStoreBundle:product:list.html.twig', ['products' => $products]);
+    }
+
+    public function createAction()
+    {
+        $product = new Product();
+        $form = $this->createFormBuilder($product)
+            ->add('title', 'text')
+            ->add('description', 'text')
+            ->add('price', 'text')
+            ->add('imageUrl', 'text')
+            ->add('Save', 'submit')->getForm();
+
+        if ('POST' == $this->getRequest()->getMethod()) {
+            $form->submit($this->getRequest());
+            $submittedProduct = $form->getData();
+            $this->getDoctrine()->getManager()->persist($submittedProduct);
+            $this->getDoctrine()->getManager()->flush();
+            return $this->redirect($this->generateUrl('products_list'));
+        }
+
+        return $this->render('BigsoftStoreBundle:product:create.html.twig', ['form' => $form->createView()]);
+
+
     }
 }
